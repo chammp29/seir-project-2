59c9cc1ada01406fc57810891ba33e108cb2c9de
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-10-04 18:27:48 -0700

    setup all controllers

---
+++ controllers/miniatures.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// import models
+const User = require("../models/User");
+const Miniature = require("../models/Miniature");
+const Paint = require("../models/Paint");
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const hbs = require("hbs");
+const parser = require("body-parser");
+const methodOverride = require("method-override");
+
+// require controllers
+const usersController = require("./controllers/users");
+const miniaturesController = require("./controllers/miniatures");
+const paintsController = require("./controllers/paints");
 
 const app = express();
-
 app.set("view engine", "hbs");
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use("/public", express.static("public"));
+
+app.use("/users/", usersController);
+app.use("/miniatures/", miniaturesController);
+app.use("/paints/", paintsController);
 
 app.listen(5000, () => console.log("Running on port 3000"));
