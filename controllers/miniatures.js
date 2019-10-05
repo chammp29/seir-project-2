ec88b825527dfafa3eced11d6f7d3f5c494fb28d
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-10-04 19:56:55 -0700

    created 'new' routes for 'miniatures' and 'paints'

--- controllers/miniatures.js
+++ controllers/miniatures.js
@@ -6,10 +6,20 @@ const User = require("../models/User");
 const Miniature = require("../models/Miniature");
 const Paint = require("../models/Paint");
 
+router.post("/", (req, res) => {
+  Miniature.create(req.body).then(newMini => {
+    res.redirect("/miniatures/");
+  });
+});
+
 router.get("/", (req, res) => {
   Miniature.find({}).then(miniatures => {
     res.render("miniatures", { miniatures });
   });
+});
+
+router.get("/new", (req, res) => {
+  res.render("new-mini");
 });
 
 router.get("/:id", (req, res) => {
--- controllers/paints.js
+++ controllers/paints.js
@@ -6,10 +6,20 @@ const User = require("../models/User");
 const Miniature = require("../models/Miniature");
 const Paint = require("../models/Paint");
 
+router.post("/", (req, res) => {
+  Paint.create(req.body).then(newPaint => {
+    res.redirect("/paints/");
+  });
+});
+
 router.get("/", (req, res) => {
   Paint.find({}).then(paints => {
     res.render("paints", { paints });
   });
+});
+
+router.get("/new", (req, res) => {
+  res.render("new-paint");
 });
 
 router.get("/:id", (req, res) => {
