a6558e83c80dbad073d69cb4c90e3365e652696e
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-10-08 16:52:16 -0700

    successfully linked a miniature to a paint

--- controllers/miniatures.js
+++ controllers/miniatures.js
@@ -18,6 +18,44 @@ router.post("/", (req, res) => {
   });
 });
 
+// test
+router.put("/paintsused/:id", (req, res) => {
+  const paintID = Object.keys(req.body)[0];
+  const miniID = req.params.id;
+
+  console.log(Object.keys(req.body)[0]);
+
+  Paint.findById(paintID).then(paint => {
+    Miniature.findOne({ _id: miniID }).then(mini => {
+      mini.paintsUsed.push(paint);
+      mini.save();
+      res.redirect(`/miniatures/paintsused/${mini.id}`);
+    });
+  });
+});
+//test
+router.get("/paintsused/:id/", (req, res) => {
+  Miniature.findOne({ _id: req.params.id })
+    .populate("paintsUsed")
+    .then(miniature => {
+      Paint.find({}).then(paints => {
+        res.render("paints-used", { miniature, paints });
+      });
+    });
+});
+//test
+// router.put("/:id/:paintId", (req, res) => {
+//   const miniID = req.params.id;
+//   const paintID = req.params.id;
+
+//   Paint.findById(paintID).then(paint => {
+//     Miniature.findOneAndUpdate({ _id: miniID }).then(mini => {
+//       mini.paintsUsed.push(paint);
+//       mini.save();
+//     });
+//   });
+// });
+
 router.put("/:id", (req, res) => {
   Miniature.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true
@@ -33,19 +71,32 @@ router.get("/", (req, res) => {
 });
 
 router.get("/edit/:id/", (req, res) => {
-  Miniature.findOne({ _id: req.params.id }).then(miniature => {
-    res.render("edit-mini", { miniature });
-  });
+  Miniature.findOne({ _id: req.params.id })
+    .populate("paintsUsed")
+    .then(miniature => {
+      res.render("edit-mini", { miniature });
+    });
 });
 
 router.get("/new", (req, res) => {
   res.render("new-mini");
 });
 
+//test
+// router.get("/:id/:paintId", (req, res) => {
+//   Miniature.findOne({ _id: req.params.id }).then(miniature => {
+//     Paint.find({}).then(paint => {
+//       res.render("paintandmini", { miniature, paint });
+//     });
+//   });
+// });
+
 router.get("/:id", (req, res) => {
-  Miniature.findOne({ _id: req.params.id }).then(mini => {
-    res.render("show-mini", mini);
-  });
+  Miniature.findOne({ _id: req.params.id })
+    .populate("paintsUsed")
+    .then(mini => {
+      res.render("show-mini", mini);
+    });
 });
 
 module.exports = router;
