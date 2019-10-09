3437dac913bb544afb15032362f1f03e7210df4c
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-10-08 19:54:46 -0700

    fixed a bug

--- db/connection.js
+++ db/connection.js
@@ -9,13 +9,11 @@ if (process.env.NODE_ENV === "production") {
   mongoURI = "mongodb://localhost/colourJournalDB";
 }
 
-mongoose.connect(
-  mongoURI,
-  { useNewUrlParser: true }
-    .then(instance =>
-      console.log(`Connected to db: ${instance.connections[0].name}`)
-    )
-    .catch(error => console.log("Connection failed", error))
-);
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(instance =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch(error => console.log("Connection failed!", error));
 
 module.exports = mongoose;
